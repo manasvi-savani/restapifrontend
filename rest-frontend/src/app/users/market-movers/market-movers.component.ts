ec959c939896295fefbc13a20f5e947d560bbb3b
Wesbert <wesberte@gmail.com>
2021-08-26 23:31:15 +0000

    moving gainers and losers to marker movers component

--- rest-frontend/src/app/users/market-movers/market-movers.component.ts
+++ rest-frontend/src/app/users/market-movers/market-movers.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RestapiService } from 'src/service/restapi.service';
 
 @Component({
   selector: 'app-market-movers',
@@ -7,9 +8,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MarketMoversComponent implements OnInit {
 
-  constructor() { }
+  reportData = {name:''}
+  reportCash = {cash:''}
+  reporttgl = {}
+  reportgl = {}
 
-  ngOnInit(): void {
+  totalNetworth:any = {category: 'users'}
+  totalcash:any = {category: 'users'}
+  todaygl:any ={category: 'users'}
+  totalgl:any = {category: 'users'}
+  top5:any = {category: 'users', name:''}
+  loser5:any = {category: 'users', name:''}
+  errorMessage = 'Result Not Found'
+  category:string = 'users'
+  name:string = ''
+
+  constructor(private restapiService:RestapiService) { }
+
+  serviceCallTotalNetworth(){
+    this.restapiService.getnetworth(this.totalNetworth).subscribe((data:any)=>{this.reportData = data})
+  }
+
+  serviceCallTotalCash(){
+    this.restapiService.getcash(this.totalcash).subscribe((data:any)=>{this.reportCash = data})
+  }
+
+  serviceCallTodayGL(){
+    this.restapiService.gettodaygl(this.todaygl).subscribe((data:any)=>{this.reporttgl = data})
+  }
+
+  serviceCallTotalGL(){
+    this.restapiService.gettotalgl(this.totalgl).subscribe((data:any)=>{this.reportgl = data})
+  }
+
+  gettop5gainers(){
+    this.restapiService.gettop5gainers({category: this.category}).subscribe((data:any)=>{this.top5 = data})
+  }
+  
+  serviceCall5loser(){
+    this.restapiService.get5loser({category: this.category}).subscribe((data:any)=>{this.loser5 = data})
+  }
+
+  ngOnInit(){
+    this.serviceCallTotalCash()
+    this.serviceCallTotalNetworth()
+    this.serviceCallTotalGL()
+    this.serviceCallTodayGL()
+    this.gettop5gainers()
+    this.serviceCall5loser()
   }
 
 }
