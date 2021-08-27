3262513d226fec7394e1117c8edb79f092f18af2
Wesbert <wesberte@gmail.com>
2021-08-27 14:15:34 +0000

    Data Entered

--- rest-frontend/src/app/users/users.component.ts
+++ rest-frontend/src/app/users/users.component.ts
@@ -50,21 +50,7 @@ export class UsersComponent implements OnInit {
     } )
   }
 
-  serviceBuyStockInRange(){
-    // we call to a service method by subscribing to it 
-    // remember the api call will be async so subscribing responds when it returns
-    this.restapiService.getBuyInRange({category: this.category, date1: this.date1, date2: this.date2}).subscribe( (data) =>{
-        this.stockInRangeBuy = data
-    } )
-  }
-
-  serviceSellStockInRange(){
-    // we call to a service method by subscribing to it 
-    // remember the api call will be async so subscribing responds when it returns
-    this.restapiService.getSellInRange({category: this.category, date1: this.date1, date2: this.date2}).subscribe( (data)=>{
-        this.stockInRangeSell = data
-    } )
-  }
+  
 
   ngOnInit(){
   
